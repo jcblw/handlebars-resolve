c262a83a2ddc039c9bf9f9ec147035de11303fca
Jacob Lowe <jlowe@agentace.com>
2015-05-31 09:48:07 -0700

    removing test in favor of using glob straight

--- test/index.js
+++ test/index.js
@@ -3,16 +3,6 @@ var
   fs = require('fs'),
   hbsResolve = require('..');
 
-test('the _getFiles method', function(t) {
-  t.equals(typeof hbsResolve._getFiles, 'function', '_getFiles is a function');
-  hbsResolve._getFiles('test/templates/*.hbs', function(err, files) {
-    t.equals(err, null, 'error is not present');
-    t.equals(Array.isArray(files), true, 'files is an array');
-    t.equals(files.length, 1, 'files has one file in it because of the glob given');
-    t.end();
-  });
-});
-
 test('the _readFile method', function(t) {
   t.equals(typeof hbsResolve._readFile, 'function', '_readFiles is a function');
   hbsResolve._readFile('test/templates/index.hbs', function(err, file) {
