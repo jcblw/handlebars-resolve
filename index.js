267449b42632e0c26bc119aa96e6f4bc4bc8a8fe
Jacob Lowe <jlowe@agentace.com>
2015-06-01 10:50:30 -0700

    adding in more pathing fixesx

--- index.js
+++ index.js
@@ -159,7 +159,12 @@ function mapPathResolve(options, files, index) {
     var
       fileName = path.relative(options.cwd, files[index][0]),
       absPath = path.resolve(options.cwd, options.basePath,  node.params[0].value),
-      relPath = './' +   path.relative(options.cwd, absPath);
+      relPath = path.relative(options.cwd, absPath);
+
+    if (!relPath.match(/^\./)) {
+      relPath = './' + relPath;
+    }
+
     return [node.loc.start, relPath, fileName];
   };
 };
